2ac6ffb4b659489b61b56f5523e85a97be461d9f
Djfaucette <daniel@nativestatedesign.co>
2016-07-16 08:22:01 -0400

    Added single book view

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectId;
 
 var router = function(nav){
 
@@ -20,21 +21,22 @@ var router = function(nav){
             });
           });
         });
-      // res.render('booksListView', {
-      //     title: 'Books',
-      //     nav: nav,
-      //     books: books
-      //   });
     });
   bookRouter.route('/:id')
-    .get(function(req, res){
-      var id = req.params.id;
-      res.render('bookView', {
-          title: 'Books',
-          nav: nav,
-          book: books[id]
+  .get(function(req, res){
+    var id = new objectId(req.params.id);
+    var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.findOne({_id: id},function(err, results){
+          res.render('bookView', {
+            title: 'Books',
+            nav: nav,
+            book: results
+          });
         });
-    });
+      });
+  });
     return bookRouter;
 };
 
