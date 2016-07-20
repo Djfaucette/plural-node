d889e720d6c86cc33fd3b6e7a9064b16a053a76c
Djfaucette <daniel@nativestatedesign.co>
2016-07-20 17:18:03 -0400

    Moved book functions to controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,48 @@
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectId;
+
+var bookController = function(bookService, nav){
+  var middleware = function(req, res, next){
+    if (!req.user) {
+      res.redirect('/');
+    } else{
+      next();
+    }
+  };
+
+  var getIndex = function(req, res){
+    var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+
+        collection.find({}).toArray(function(err, results){
+          res.render('booksListView', {
+            title: 'Books',
+            nav: nav,
+            books: results
+          });
+        });
+      });
+  };
+  var getById = function(req, res){
+    var id = new objectId(req.params.id);
+    var url = 'mongodb://localhost:27017/libraryApp';
+      mongodb.connect(url, function(err, db){
+        var collection = db.collection('books');
+        collection.findOne({_id: id},function(err, results){
+          res.render('bookView', {
+            title: 'Books',
+            nav: nav,
+            book: results
+          });
+        });
+      });
+    };
+  return {
+    getIndex: getIndex,
+    getById: getById,
+    middleware: middleware
+  };
+};
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,45 +4,16 @@ var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectId;
 
+
 var router = function(nav){
+var bookController = require('../controllers/bookController')(null, nav);
+  bookRouter.use(bookController.middleware);
 
-  bookRouter.use(function(req, res, next){
-    if (!req.user) {
-      res.redirect('/');
-    } else{
-      next();
-    }
-  })
   bookRouter.route('/')
-    .get(function(req, res){
-      var url = 'mongodb://localhost:27017/libraryApp';
-        mongodb.connect(url, function(err, db){
-          var collection = db.collection('books');
+    .get(bookController.getIndex);
 
-          collection.find({}).toArray(function(err, results){
-            res.render('booksListView', {
-              title: 'Books',
-              nav: nav,
-              books: results
-            });
-          });
-        });
-    });
   bookRouter.route('/:id')
-  .get(function(req, res){
-    var id = new objectId(req.params.id);
-    var url = 'mongodb://localhost:27017/libraryApp';
-      mongodb.connect(url, function(err, db){
-        var collection = db.collection('books');
-        collection.findOne({_id: id},function(err, results){
-          res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: results
-          });
-        });
-      });
-  });
+  .get(bookController.getById);
     return bookRouter;
 };
 
